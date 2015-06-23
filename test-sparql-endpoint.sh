c1a45a2d53ef7a5e4de6de244ab6d049b821fa74
MISHIMA Hiroyuki <missy@be.to>
2015-06-23 11:53:42 +0900

    update test-sparql-endpoint to test GO

--- test-sparql-endpoint.sh
+++ test-sparql-endpoint.sh
@@ -5,6 +5,7 @@ graph="http://misshie.jp/rdf/test-rdfxml"
 #format="text/turtle"
 format="text/tab-separated-values"
 
+echo ">>> Test HPO"
 query=`cat <<EOF
 SELECT "*"
 FROM <http://purl.obolibrary.org/obo/hp.owl> 
@@ -12,5 +13,14 @@ WHERE { ?s ?p ?o . }
 LIMIT 10
 EOF
 `
+eval curl --form "\"format="${format}"\"" --form "\"query="${query}"\"" ${url}
 
+echo ">>> TEST GO"
+query=`cat <<EOF
+SELECT "*"
+FROM <http://purl.obolibrary.org/obo/go/go.owl> 
+WHERE { ?s ?p ?o . }
+LIMIT 10
+EOF
+`
 eval curl --form "\"format="${format}"\"" --form "\"query="${query}"\"" ${url}
